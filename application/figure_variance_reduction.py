5e014d50bbb7e42a5a1cc25daafbc968370c4c5f
Ariane Ducellier <ducela@uw.edu>
2026-01-15 14:01:33 -0800

    modify figures for application

---
+++ application/figure_variance_reduction.py
@@ -0,0 +1,40 @@
+import altair as alt
+import numpy as np
+import pandas as pd
+import pickle
+
+pd.options.mode.chained_assignment = None
+
+with open('results_25.pkl', 'rb') as fp:
+    [df, Dphi_y, Dphi_s, sigma] = pickle.load(fp)
+df['value'] = df['value'] / df['upper']
+df['raked_value'] = df['raked_value'] / df['upper']
+df['std'] = df['std'] / df['upper']
+df['raked_std'] = np.sqrt(df['std']) / df['upper']
+
+min_value = min(df['raked_std'].min(), df['std'].min())
+max_value = max(df['raked_std'].max(), df['std'].max())
+
+diagonal = alt.Chart().mark_rule(strokeDash=[8, 8]).encode(
+    x=alt.value(0),
+    x2=alt.value('width'),
+    y=alt.value('height'),
+    y2=alt.value(0)
+)
+mx = alt.Chart().mark_point(size=80, filled=False).encode(
+    x=alt.X('std:Q', axis=alt.Axis(title='Standard deviation before raking', format='.1e'), scale=alt.Scale(domain=[min_value, max_value], type='log')),
+    y=alt.Y('raked_std:Q', axis=alt.Axis(title='Standard deviation after raking', format='.1e'), scale=alt.Scale(domain=[min_value, max_value], type='log'))
+)
+chart = alt.layer(
+    diagonal,
+    mx,
+    data=df
+).configure_axis(
+    labelFontSize=14,
+    titleFontSize=14
+).configure_legend(
+    labelFontSize=14,
+    titleFontSize=14
+)
+chart.save('variance_reduction.svg')
+
---
+++ application/figure_workflows_boxplot.py
@@ -0,0 +1,27 @@
+import altair as alt
+import numpy as np
+import pandas as pd
+import pickle
+
+pd.options.mode.chained_assignment = None
+
+with open('results_25.pkl', 'rb') as fp:
+    [df1, Dphi_y, Dphi_s, sigma] = pickle.load(fp)
+df1['Workflow'] = '1 step'
+
+with open('results_25_4steps.pkl', 'rb') as fp:
+    df2 = pickle.load(fp)
+df2['Workflow'] = '4 steps'
+
+df = pd.concat([df1, df2])
+
+df['value'] = df['value'] / df['upper']
+df['raked_value'] = df['raked_value'] / df['upper']
+df['Percentage change'] = np.abs((df['raked_value'] - df['value']) / df['value'])
+
+chart = alt.Chart(df).mark_boxplot().encode(
+    x=alt.X('Percentage change:Q', scale=alt.Scale(zero=False)),
+    y=alt.Y('Workflow:N')
+)
+chart.save('comparison_workflows_boxplot.svg')
+
